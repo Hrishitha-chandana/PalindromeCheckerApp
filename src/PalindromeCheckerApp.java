745578dd1481349054c0f1369e6a31bed0c2996f
Hrishitha-chandana <hrishitha268@gmail.com>
2026-02-26 15:13:16 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -24,7 +24,7 @@ public class PalindromeCheckerApp {
         String reversedHardcoded = "";
 
         for (int i = hardcoded.length() - 1; i >= 0; i--) {
-            reversedHardcoded = reversedHardcoded + hardcoded.charAt(i);
+            reversedHardcoded += hardcoded.charAt(i);
         }
 
         if (hardcoded.equals(reversedHardcoded)) {
@@ -35,23 +35,51 @@ public class PalindromeCheckerApp {
 
 
         // ==========================
-        // UC3: User Input Palindrome
+        // UC3: Reverse String Method
         // ==========================
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("\nEnter a word to check palindrome: ");
+        System.out.print("\nEnter a word for UC3 check: ");
         String input = sc.nextLine();
 
-        String reversedInput = "";
+        String reversed = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
-            reversedInput = reversedInput + input.charAt(i);
+            reversed += input.charAt(i);
         }
 
-        if (input.equals(reversedInput)) {
+        if (input.equals(reversed)) {
             System.out.println("UC3 Result: " + input + " is a palindrome.");
         } else {
             System.out.println("UC3 Result: " + input + " is not a palindrome.");
+        }
+
+
+        // ==========================
+        // UC4: Character Array + Two Pointer
+        // ==========================
+        System.out.print("\nEnter a word for UC4 check: ");
+        String word = sc.nextLine();
+
+        char[] arr = word.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("UC4 Result: " + word + " is a palindrome.");
+        } else {
+            System.out.println("UC4 Result: " + word + " is not a palindrome.");
         }
 
         sc.close();
